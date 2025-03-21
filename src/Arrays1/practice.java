9cf08c69d3a19b05d46bc1ccd8f96e20113f3607
chaithrakit2025 <chaithra.kit@gmail.com>
2025-03-21 13:36:00 -0400

    updated code

--- src/Arrays1/practice.java
+++ src/Arrays1/practice.java
@@ -7,7 +7,7 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-				int[] numbers = {1,2,3,4,5,6};
+				int[] numbers = {1,2,3,4,5,6, 7,8,9,10};
 				System.out.println(" the numbers are" + Arrays.toString(numbers));
 
 			}
