423541dcbcd6ebd1023cfd0dfff24a3f496314e2
chaithrakit2025 <chaithra.kit@gmail.com>
2025-03-21 13:32:36 -0400

    Adding Array jave project

---
+++ src/Arrays1/practice.java
@@ -0,0 +1,18 @@
+package Arrays1;
+
+import java.util.Arrays;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				int[] numbers = {1,2,3,4,5,6};
+				System.out.println(" the numbers are" + Arrays.toString(numbers));
+
+			}
+
+
+	}
+
+
